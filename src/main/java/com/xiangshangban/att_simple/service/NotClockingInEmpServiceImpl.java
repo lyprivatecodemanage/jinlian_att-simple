78afc60638651cba9bafd8416270b5cdb8d94a65
wyh <lujiang@xiangshangban.com>
2018-01-23 14:33:48 +0800

    wyh@2018-01-23更改人员班次列表，和无需考勤人员接口

--- src/main/java/com/xiangshangban/att_simple/service/NotClockingInEmpServiceImpl.java
+++ src/main/java/com/xiangshangban/att_simple/service/NotClockingInEmpServiceImpl.java
@@ -25,6 +25,9 @@ public class NotClockingInEmpServiceImpl implements NotClockingInEmpService{
 	@Autowired
 	private NotClockingInEmpMapper notClockingInEmpMapper;
 	
+	/**
+	 * （添加/更新/删除）无需考勤人员
+	 */
 	@Override
 	public boolean addNotClockingInEmp(String requestParam, String companyId) {
 		
@@ -36,37 +39,54 @@ public class NotClockingInEmpServiceImpl implements NotClockingInEmpService{
 		boolean result = false;
 		
 		if(object!=null){
+			JSONArray empArray = JSONArray.parseArray(JSONArray.toJSONString(object));
 			if(operateFlag.equals("1")){
 				//执行删除当前公司无需考勤人员操作
 				notClockingInEmpMapper.removeNotClockingInEmp(companyId.trim());
-			}
-			JSONArray empArray = JSONArray.parseArray(JSONArray.toJSONString(object));
-			if(empArray.size()>0){
-				for(int i=0;i<empArray.size();i++){
-					
-					JSONObject emp = JSONObject.parseObject(JSONArray.toJSONString(empArray.get(i)));
-					
-					NotClockingInEmp notClockingInEmp = new NotClockingInEmp();
-					notClockingInEmp.setId(FormatUtil.createUuid());
-					notClockingInEmp.setEmpId(emp.getString("empId"));
-					notClockingInEmp.setEmpCompanyId(companyId);
-					
-					int insertResult = notClockingInEmpMapper.insertNotClockingInEmp(notClockingInEmp);
-					
-					if(insertResult>0){
-						result = true;
-					}else{
-						result = false;
-					}
+				if(empArray.size()==0){ //仅仅执行删除公司人员操作
+					result = true;
 				}
+			}
+			if(empArray.size()>0){
+				result = commonAddNotClockingInEmp(empArray,companyId);
 			}
 		}
 		return result;
 	}
-
+	
+	/**
+	 * 查询无需考勤人员
+	 */
 	@Override
 	public List<Map> queryNotClockingInEmp(String companyId) {
 		List<Map> selectNotClockingInEmp = notClockingInEmpMapper.selectNotClockingInEmp(companyId);
 		return selectNotClockingInEmp;
+	}
+	
+	/*********************************************************
+	 * 添加无需考勤人员公共操作
+	 *********************************************************/
+	
+	public boolean commonAddNotClockingInEmp(JSONArray empArray,String companyId){
+		//初始化返回结果
+		boolean result = false;
+		for(int i=0;i<empArray.size();i++){
+			
+			JSONObject emp = JSONObject.parseObject(JSONArray.toJSONString(empArray.get(i)));
+			
+			NotClockingInEmp notClockingInEmp = new NotClockingInEmp();
+			notClockingInEmp.setId(FormatUtil.createUuid());
+			notClockingInEmp.setEmpId(emp.getString("empId"));
+			notClockingInEmp.setEmpCompanyId(companyId);
+			
+			int insertResult = notClockingInEmpMapper.insertNotClockingInEmp(notClockingInEmp);
+			
+			if(insertResult>0){
+				result = true;
+			}else{
+				result = false;
+			}
+		}
+		return result;
 	}
 }
