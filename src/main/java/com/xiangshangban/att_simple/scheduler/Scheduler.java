f8a4dc9d42a2813816a006f722b92cc040aaf66a
weiyoudi <742650941@qq.com>
2018-03-01 15:19:09 +0800

    定时任务测试用

--- src/main/java/com/xiangshangban/att_simple/scheduler/Scheduler.java
+++ src/main/java/com/xiangshangban/att_simple/scheduler/Scheduler.java
@@ -20,7 +20,7 @@ public class Scheduler {
 	//每天凌晨1点执行
 	private static final String REPORT_TIME = "0 0 1 * * ?";//日报计算时间
 	//每天凌晨0点执行
-	private static final String REPORT_MONTH = "30 50 18 * * ?";//月报计算时间
+	private static final String REPORT_MONTH = "10 23 10 * * ?";//月报计算时间
 	private final Logger logger = Logger.getLogger(Scheduler.class);
 	
 	@Autowired
@@ -55,7 +55,7 @@ public class Scheduler {
 	public void countReportMonth(){
 		logger.info("月报计算开始时间"+TimeUtil.getCurrentTime()); 
 		String countDate = TimeUtil.getCurrentDate();
-		algorithmService.calculate("3F0779E7F0584048BDBB67505938BB54", "8A75ADB4F16E453CBB302263A98FE9B5", "2018-02-27");
+		algorithmService.calculate("001F15DE6BB64656B5F35FD9112F7DDB", "37EB363339AF405B81FB33E83C259C91", "2018-02-28");
 		
 		//algorithmService.calculate("1DC63D0B6AFC41918AB18F61EBC49E8F", "297AD69AB0274BBC91813689C15FFDDB", "2018-01-31", "2018-01-31");
 		//algorithmService.calculateByCompany("D8640545612F4973A70CCF1019A02F6C", "2018-01-01", "2018-02-07");
