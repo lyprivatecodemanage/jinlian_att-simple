f51884565e03f1ce0aaf2345e4c5123261235ecb
weiyoudi <742650941@qq.com>
2018-03-06 18:12:53 +0800

    删除注释

--- src/main/java/com/xiangshangban/att_simple/controller/TestController.java
+++ src/main/java/com/xiangshangban/att_simple/controller/TestController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.xiangshangban.att_simple.service.AlgorithmService;
 import com.xiangshangban.att_simple.utils.TimeUtil;
 
