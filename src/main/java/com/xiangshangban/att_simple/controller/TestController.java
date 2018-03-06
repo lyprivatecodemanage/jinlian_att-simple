6738368e92d2eacb782702b366553461eefa8676
weiyoudi <742650941@qq.com>
2018-03-06 18:06:07 +0800

    加入测试触发接口

---
+++ src/main/java/com/xiangshangban/att_simple/controller/TestController.java
@@ -0,0 +1,68 @@
+package com.xiangshangban.att_simple.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.xiangshangban.att_simple.service.AlgorithmService;
+import com.xiangshangban.att_simple.utils.TimeUtil;
+
+/**
+ * 用于手动触发某个方法
+ * @author Administrator
+ *
+ */
+@RestController
+@RequestMapping("/JNTest")
+public class TestController {
+	private static final Logger logger = Logger.getLogger(ApplicationController.class);
+	@Autowired
+	private AlgorithmService algorithmService;
+	/**
+	 * 计算日报
+	 * @param companyId
+	 * @param employeeId
+	 * @param beginDate
+	 * @param endDate
+	 * @param type 0：具体某个公司某个人员一段区间的日报计算，1：某个公司的所有人员一段区间的日报计算，2：所有公司所有人一段区间日报计算
+	 * @return
+	 */
+	@RequestMapping(value = "/countReport",produces="application/json;charset=utf-8",method=RequestMethod.POST)
+	public  Map<String,Object> applicationIndexPage(@RequestParam("companyId") String companyId, 
+			@RequestParam("employeeId") String employeeId, 
+			@RequestParam("beginDate") String beginDate, @RequestParam("endDate") String endDate,
+			@RequestParam("type") String type){
+		logger.info("接口手动触发开始:companyId="+companyId+",employeeId"+employeeId+",beginDate="+beginDate+",endDate="+endDate); 
+		switch (type) {
+		case "0":
+			algorithmService.calculate(companyId, employeeId, beginDate, endDate);
+			break;
+		case "1":
+			algorithmService.calculate(companyId, beginDate, endDate);
+			break;
+		case "2":
+			algorithmService.calculate(beginDate, endDate);
+			break;
+		default:
+			break;
+		}
+		logger.info("接口手动触发完成");
+		logger.info("月报计算完成时间"+TimeUtil.getCurrentTime()); 
+		Map<String,Object> result = new HashMap<String,Object>();
+		result.put("message", "触发计算成功:companyId="+companyId+",employeeId"+employeeId+",beginDate="+beginDate+",endDate="+endDate);
+		result.put("returnCode", "3000");
+		return result;
+	}
+}
--- src/main/java/com/xiangshangban/att_simple/scheduler/Scheduler.java
+++ src/main/java/com/xiangshangban/att_simple/scheduler/Scheduler.java
@@ -20,7 +20,7 @@ public class Scheduler {
 	//每天凌晨1点执行
 	private static final String REPORT_TIME = "0 0 1 * * ?";//日报计算时间
 	//每天凌晨0点执行
-	private static final String REPORT_MONTH = "15 16 16 * * ?";//月报计算时间
+	private static final String REPORT_MONTH = "30 55 16 * * ?";//月报计算时间
 	private final Logger logger = Logger.getLogger(Scheduler.class);
 	
 	@Autowired
@@ -51,16 +51,16 @@ public class Scheduler {
 	/**
 	 * 月报计算（测试用）
 	 */
-	@Scheduled(cron=REPORT_MONTH)
+	//@Scheduled(cron=REPORT_MONTH)
 	public void countReportMonth(){
 		logger.info("月报计算开始时间"+TimeUtil.getCurrentTime()); 
 		String countDate = TimeUtil.getCurrentDate();
 		//algorithmService.calculate("001F15DE6BB64656B5F35FD9112F7DDB", "37EB363339AF405B81FB33E83C259C91", "2018-03-01");
 		
-		algorithmService.calculate("001F15DE6BB64656B5F35FD9112F7DDB", "20A215C019864D1684316F4A8A5EE22E", "2018-03-05", "2018-03-05");
+		//algorithmService.calculate("001F15DE6BB64656B5F35FD9112F7DDB", "20A215C019864D1684316F4A8A5EE22E", "2018-03-05", "2018-03-05");
 		//algorithmService.calculateByCompany("CBBF109CBECF4DE0BA0604E395532902", "2018-02-01", "2018-02-28");
 		//algorithmService.calculateMonth(countDate);//计算当前日期之后月报的应出勤
-		//algorithmService.calculate("2018-02-01", "2018-03-02");
+		algorithmService.calculate("2018-03-05", "2018-03-05");
 		logger.info("月报计算完成");
 		logger.info("月报计算完成时间"+TimeUtil.getCurrentTime()); 
 	}
